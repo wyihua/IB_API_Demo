3fa799c4694e263b483821284421e1bf8bbc92a0
dauphinwill <seu.wuqihong@gmail.com>
2018-01-17 22:05:34 -0800

    尝试调用c++的库

---
+++ api/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "../twsapi_macunix.972.18/IBJts/source/CppClient/client/EClient.h"
+
+using namespace std;
+
+int main() 
+{
+    cout << "Hello, World!\n";
+    return 0;
+}
