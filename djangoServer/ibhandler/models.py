daec92356ad9b4cfe45c8470d51a2357fb8268e9
dauphinwill <seu.wuqihong@gmail.com>
2018-01-20 15:17:26 -0800

    model initialization

--- djangoServer/ibhandler/models.py
+++ djangoServer/ibhandler/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Ticket(models.Model):
+    ask_size = models.IntegerField
+    bit_size = models.IntegerField
+    last_size = models.IntegerField
+    bid_price = models.IntegerField
+    close_price = models.IntegerField
+
+
+
