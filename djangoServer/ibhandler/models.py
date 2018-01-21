f2fd3ea5882481a97109074fb6aaa8dd8ba48525
dauphinwill <seu.wuqihong@gmail.com>
2018-01-20 16:05:03 -0800

    解决数据库没有field的问题

--- djangoServer/ibhandler/models.py
+++ djangoServer/ibhandler/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Ticket(models.Model):
-    ask_size = models.IntegerField
-    bit_size = models.IntegerField
-    last_size = models.IntegerField
-    bid_price = models.IntegerField
-    close_price = models.IntegerField
+    ask_size = models.IntegerField(default=0)
+    bit_size = models.IntegerField(default=0)
+    last_size = models.IntegerField(default=0)
+    bid_price = models.IntegerField(default=0)
+    close_price = models.IntegerField(default=0)
 
 
 
